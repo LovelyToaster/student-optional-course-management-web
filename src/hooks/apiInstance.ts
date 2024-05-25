380a95dc8887c2ce9a1099cc144889691973dff1
LovelyToaster94 <LovelyToaster94@outlook.com>
2024-05-25 16:22:04 +0800

    feat: 为axios添加当token错误时的拦截器

--- src/hooks/apiInstance.ts
+++ src/hooks/apiInstance.ts
@@ -1,8 +1,21 @@
 import axios from "axios";
+import {errorNotification} from "./notification";
 
 const apiInstance = axios.create({
     baseURL: "http://localhost:8080"
 })
 apiInstance.defaults.withCredentials = true
-apiInstance.defaults.headers.common["Content-Type"]="application/x-www-form-urlencoded"
+apiInstance.defaults.headers.common["Content-Type"] = "application/x-www-form-urlencoded"
+apiInstance.interceptors.response.use((response) => {
+    return response
+}, (error) => {
+    if (error.response.status === 301 && window.location.pathname != "/") {
+        let url = window.location.origin
+        errorNotification("Token错误或已过期,稍后会返回登录界面")
+        setTimeout(() => {
+            window.location.replace(url)
+        }, 1500)
+    }
+    return Promise.reject(error)
+})
 export default apiInstance
