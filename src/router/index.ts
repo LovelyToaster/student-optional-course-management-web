4f32f7349b5021f6f45432987d51731ef8171426
LovelyToaster94 <LovelyToaster94@outlook.com>
2024-06-04 10:26:42 +0800

    feat: 学生添加信息功能

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,7 @@ import code from "../hooks/code";
 import CourseManagement from "../views/management/info/CourseManagement.vue";
 import GradeManagement from "../views/management/info/GradeManagement.vue";
 import TeacherAdd from "../views/management/info/TeacherAdd.vue";
+import StudentAdd from "../views/management/info/StudentAdd.vue";
 
 async function loginStatus() {
     let loginStore = useLoginStore()
@@ -90,6 +91,14 @@ const router = createRouter({
                             path: "teacherAdd",
                             name: "teacherAdd",
                             component: TeacherAdd,
+                            meta: {
+                                isVerify: true
+                            }
+                        },
+                        {
+                            path: "studentAdd",
+                            name: "studentAdd",
+                            component: StudentAdd,
                             meta: {
                                 isVerify: true
                             }
---
+++ src/views/management/info/StudentAdd.vue
@@ -0,0 +1,102 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import {StudentInfoInter} from "@/types";
+import apiInstance from "@/hooks/apiInstance";
+import code from "@/hooks/code";
+import {errorNotification, successNotification} from "@/hooks/notification";
+
+let addInfoStep = {
+  studentNo: undefined,
+  studentName: undefined,
+  studentSex: undefined,
+  studentAge: undefined,
+  studentFaculties: undefined,
+  studentClass: undefined,
+}
+let addInfo = ref<StudentInfoInter>({...addInfoStep})
+
+function confirmAddInfo() {
+  apiInstance.post("/student/add", addInfo.value)
+      .then((resp) => {
+        let addResp = resp.data
+        if (addResp.code === code.ADD_SUCCESS) {
+          successNotification(addResp.message)
+          addInfo.value = addInfoStep
+        } else {
+          errorNotification(addResp.message)
+        }
+      })
+}
+</script>
+
+<template>
+  <div class="main">
+    <h1>添加学生信息</h1>
+    <div class="add">
+      <form action="#" @submit.prevent>
+        <div>
+          <span>学生姓名：</span>
+          <input type="text" required v-model="addInfo.studentName">
+        </div>
+        <div>
+          <span>学生性别：</span>
+          <input type="radio" name="teacherSex" value="男" required v-model="addInfo.studentSex">男
+          <input type="radio" name="teacherSex" value="女" required v-model="addInfo.studentSex">女
+        </div>
+        <div>
+          <span>学生年龄：</span>
+          <input type="number" min="18" max="80" required v-model="addInfo.studentAge">
+        </div>
+        <div>
+          <span>学生院系：</span>
+          <input type="text" required v-model="addInfo.studentFaculties">
+        </div>
+        <div>
+          <span>学生班级：</span>
+          <input type="text" required v-model="addInfo.studentClass">
+        </div>
+        <div class="add-button">
+          <input type="submit" @click="confirmAddInfo">
+          <input type="reset">
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.add {
+  margin-top: 10px;
+}
+
+.add > form {
+  margin-top: 10px;
+}
+
+.add > form > div {
+  margin-top: 10px;
+}
+
+.add > form > div > span {
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.add-button {
+  display: flex;
+  justify-content: space-evenly;
+  margin-top: 20px;
+}
+
+.add-button > input {
+  height: 30px;
+  width: 50px;
+}
+</style>
